430fa313abcba314244c8953b5d144f752f9eebc
soujangadi <ts-soujanya.angadi@10.99.63.4>
2023-05-25 12:27:25 +0530

    New Project - BasicCalci

---
+++ src/simplecalsi.java
@@ -0,0 +1,41 @@
+
+public class simplecalsi {
+	static void add(int a,int b) {
+	System.out.println(a+b);
+	}
+	
+	static void sub(int a,int b) {
+	System.out.println( a-b);
+	}
+	
+	static void mul(int a,int b) {
+	System.out.println( a*b);
+	}
+	
+	static void div(int a,int b) {
+	System.out.println(a/b);}
+	
+	
+	static void mod(int a,int b) {
+	System.out.println(a%b);
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		add(10,20);
+		sub(10,20);
+		mul(10,20);
+	
+		div(10,20);
+		
+
+		
+		mod(10,20);
+		
+
+	}
+
+}
